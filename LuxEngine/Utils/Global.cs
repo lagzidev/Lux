dca969a27dc349bd1bbf4648d3ee8d860e138dd9
Omer Lagziel <omerlagziel7@gmail.com>
2020-02-21 15:35:52 +0200

    Added utils from an online tutorial, should be reviewed; Made LuxEngine a library and created a game project that uses it.

---
+++ LuxEngine/Utils/Global.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.Xna.Framework;
+
+namespace LuxEngine
+{
+    static class Global
+    {
+        public static LuxGame game;
+        public static Random random = new Random();
+        public static string levelName;
+
+        public static void Initialize(LuxGame inputGame)
+        {
+            game = inputGame;
+        }
+    }
+}
