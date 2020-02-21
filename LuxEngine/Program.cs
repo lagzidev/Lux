e3964488f8c1ae00471fdc1e132c3e221d715f4e
Omer Lagziel <omerlagziel7@gmail.com>
2020-02-21 13:36:13 +0200

    First commit

---
+++ LuxEngine/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Microsoft.Xna.Framework;
+
+namespace LuxEngine
+{
+    static class Program
+    {
+        static void Main(string[] args)
+        {
+            using (Game g = new Game())
+            {
+                new GraphicsDeviceManager(g);
+                g.Run();
+            }
+        }
+    }
+}
